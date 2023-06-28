1651e50f6a56e44ba318b848db3da998a1abf85c
PolinaK <polina.kirichencko@yandex.ru>
2023-06-28 14:47:56 +0400

    deleting test_items

--- browser_language/test_items.py
+++
@@ -1,9 +0,0 @@
-import time
-from selenium.webdriver.common.by import By
-
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-
-def test_guest_should_see_button(browser):
-    browser.get(link)
-    assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket"), 'Кнопки добавления в корзину нет'
-    time.sleep(30)
